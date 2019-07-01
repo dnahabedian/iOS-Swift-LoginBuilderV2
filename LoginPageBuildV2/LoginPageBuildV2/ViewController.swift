f0e0c9bbd7abcc309d7f0a50c5db4910538baac3
Daron Nahabedian <nahabediand@gmail.com>
2019-06-30 23:54:12 -0700

    Add files via upload

---
+++ LoginPageBuildV2/LoginPageBuildV2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginPageBuildV2
+//
+//  Created by Daron S. Nahabedian on 6/30/19.
+//  Copyright © 2019 Daron Nahabedian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
